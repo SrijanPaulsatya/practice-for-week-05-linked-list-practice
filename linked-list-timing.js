5c7dfa7b7846364f05fd0e52343634f102eee87c
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2024-12-02 16:32:00 +0530

    Implement linked-list-timing

--- linked-list-timing.js
+++ linked-list-timing.js
@@ -7,3 +7,36 @@ Construct a timing test to verify the time complexities of `addToHead` and
 */
 
 // Your code here
+
+function testAddToHead(listType) {
+    const list = new listType();
+    console.time('addToHead');
+    for (let i = 0; i < 100000; i++) {
+        list.addToHead(i);
+    }
+    console.timeEnd('addToHead');
+}
+
+function testAddToTail(listType) {
+    const list = new listType();
+    console.time('addToTail');
+    for (let i = 0; i < 100000; i++) {
+        list.addToTail(i);
+    }
+    console.timeEnd('addToTail');
+}
+
+// Testing singly linked list
+console.log('Singly Linked List Add To Head Test');
+testAddToHead(LinkedList);
+
+console.log('Singly Linked List Add To Tail Test');
+testAddToTail(LinkedList);
+
+// Testing doubly linked list
+console.log('Doubly Linked List Add To Head Test');
+testAddToHead(DoublyLinkedList);
+
+console.log('Doubly Linked List Add To Tail Test');
+testAddToTail(DoublyLinkedList);
+
